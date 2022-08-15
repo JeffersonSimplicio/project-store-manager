e59ca942b662f34f6e02c553e52b3e75c7de6266
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-15 11:22:51 -0300

    feat: criada função de model getAll para vendas

--- models/salesModel.js
+++ models/salesModel.js
@@ -16,7 +16,30 @@ async function linkBuyProducts(saleId, { productId, quantity }) {
   );
 }
 
+async function getAll() {
+    const [products] = await connection.execute(
+      `SELECT
+        s.id AS saleId,
+        s.date AS 'date',
+        sp.product_id AS productId,
+        sp.quantity AS quantity
+      FROM
+        StoreManager.sales_products AS sp
+      INNER JOIN
+        StoreManager.sales AS s
+      ON
+        sp.sale_id = s.id
+      ORDER BY
+        saleId ASC,
+        productId ASC ;`,
+    );
+    return products;
+}
+
+// getAll().then((teste) => console.log(teste));
+
 module.exports = {
+  getAll,
   addSale,
   linkBuyProducts,
 };
