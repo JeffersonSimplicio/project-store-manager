b0dad8b806c764434e093d93e7229d917a574e19
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-16 12:24:37 -0300

    Req12: OK, feat: criada rota para remover sale

--- routes/salesRoute.js
+++ routes/salesRoute.js
@@ -7,11 +7,13 @@ const rescue = require('../utils/rescue');
 const salesController = require('../controllers/salesController');
 const validation = require('../middlewares/validationNewSale');
 
-router.post('/',
-  rescue(validation.validationNewSale),
-  rescue(salesController.newSale));
-
 router.get('/', salesController.getAll);
 router.get('/:id', salesController.getById);
+router.post(
+  '/',
+  rescue(validation.validationNewSale),
+  rescue(salesController.newSale),
+);
+router.delete('/:id', salesController.remove);
 
 module.exports = router; 
